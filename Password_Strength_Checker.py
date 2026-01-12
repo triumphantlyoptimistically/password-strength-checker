3bca2f186b9f596fdf5a5fcc7ac6baf2dba81078
Dylan Speakman <24DS0790@barton.ac.uk>
2026-01-12 21:18:32 +0000

    Add program skeleton and input loop

--- Password_Strength_Checker.py
+++ Password_Strength_Checker.py
@@ -1,1 +1,14 @@
-
+running = True
+while running:
+    password = input("Enter password: ")
+    validChoice = False
+    while validChoice == False:
+        checkAgain = input("Do you want to check another password? Y\\N ")
+        if checkAgain == "N":
+            validChoice = True
+            running = False
+        elif checkAgain == "Y":
+            validChoice = True
+        else:
+            print("Please enter Y\\N")
+            validChoice = False
