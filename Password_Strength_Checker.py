83f51eef35c1adcb7b98fc33345f39311c3a9559
Dylan Speakman <24DS0790@barton.ac.uk>
2026-01-12 21:26:24 +0000

    Add password length validation

--- Password_Strength_Checker.py
+++ Password_Strength_Checker.py
@@ -1,14 +1,28 @@
-running = True
-while running:
-    password = input("Enter password: ")
+def Go_Again():
     validChoice = False
     while validChoice == False:
         checkAgain = input("Do you want to check another password? Y\\N ")
         if checkAgain == "N":
-            validChoice = True
-            running = False
+            return False
         elif checkAgain == "Y":
-            validChoice = True
+            return True
         else:
             print("Please enter Y\\N")
             validChoice = False
+
+def Check_Length(password):
+    if len(password) >= 8:
+        return True
+    else:
+        return False
+
+running = True
+while running:
+    rulesFailed = []
+    password = input("Enter password: ")
+    score = 0
+    if Check_Length(password) == True:
+        score = score + 1
+    else:
+        rulesFailed.append("Length rule")
+    running = Go_Again()
